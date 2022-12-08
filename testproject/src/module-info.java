c2db366a227646dd437ccc213a613fe0f8df32a3
eunseo0423 <kyee0423@gmail.com>
2022-12-08 18:32:40 +0900

    commit test

---
+++ testproject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author KOSA
+ *
+ */
+module testproject {
+}
---
+++ testproject/src/testpackage/testclass.java
@@ -0,0 +1,8 @@
+package testpackage;
+
+public class testclass {
+
+	public static void main(String[] args) {
+		
+	}
+}
